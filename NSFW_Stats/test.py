51292f5f023f208fc2b9f914b8e2ee82ce8f8693
eye0212 <97141338+eye0212@users.noreply.github.com>
2025-10-20 06:51:20 -0400

    Delete NSFW_Stats directory

--- NSFW_Stats/test.py
+++
@@ -1,7 +0,0 @@
-import praw
-reddit = praw.Reddit(
-    client_id="b7mj-WzdiFN_YxV9T5Yqvw",
-    client_secret="XglGnNmkaDVVI6nhCTNJcHhWmyQ",
-    user_agent="reddit-citation-audit by u/eug0212"
-)
-print(reddit.subreddit("AskReddit").display_name)
